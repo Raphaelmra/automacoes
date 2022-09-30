1997b59d15daf25cedfcd092c3fb6d1144c95fe9
unknown <rmra2202@gmail.com>
2022-09-30 13:49:59 -0300

    adicao cenario cadastro

--- projpessoal/cypress/integration/AutomationPractice/Cadastro.js
+++ projpessoal/cypress/integration/AutomationPractice/Cadastro.js
@@ -5,17 +5,33 @@
 describe('example to-do app', () => {
     beforeEach(() => {
 
-        cy.visit('https://www.amazon.com.br/')
+        cy.visit('http://automationpractice.com/')
     })
 
     it('displays two todo items by default', () => {
-        cy.get("#nav-link-accountList-nav-line-1").click()
-        cy.get("#createAccountSubmit").click()
-        cy.get('#ap_customer_name').type("Raphael")
-        cy.get('#ap_email').type("rognomemle@vusra.com")
-        cy.get('#ap_password').type("teste.123")
-        cy.get('#ap_password_check').type("teste.123")
-        cy.get('#continue').click()
+        cy.get(".login").click()
+        cy.get('#email_create').type("emailtestemathias@teste.com.br")
+        cy.get('#SubmitCreate > span').click()
+        cy.wait(2000)
+        cy.get('#id_gender1').check()
+        cy.get('#customer_firstname').type("Raphael")
+        cy.get('#customer_lastname').type("Mathias")
+        cy.get('#passwd').type("teste.123")
+        cy.get('#days').select("22")
+        cy.get('#months').select("2")
+        cy.get('#years').select("2002")
+        cy.get('#firstname').type("Raphael")
+        cy.get('#id_state').select("2")
+        cy.get('#lastname').type("Mathias")
+        cy.get('#address1').type("Rua a")
+        cy.get('#address2').type("casa 2")
+        cy.get('#city').type("Alabama")
+        cy.get('#postcode').type("89115")
+
+        cy.get('#phone').type("21971668476")
+        cy.get('#phone').type("21971668476")
+        cy.get('#alias').type("Endereco teste")
+        cy.get('#submitAccount > span').click()
     })
 
 
